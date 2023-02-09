111fe58b66af8e4b002fbc796a150ac2b7461397
lujainhasnan <121128316+lujainhasnan@users.noreply.github.com>
2023-02-09 13:17:32 +0300

    Create main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import cv2
+import os
 import pickle
 import face_recognition
 import cvzone
@@ -7,7 +8,7 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 from firebase_admin import storage
-
+from datetime import datetime
 
 # Initialize the Firebase app
 cred = credentials.Certificate("serviceAccountKey.json")
@@ -15,6 +16,7 @@ firebase_admin.initialize_app(cred, {
     'databaseURL':"https://mawjudfirebase-default-rtdb.firebaseio.com/",
     'storageBucket':"mawjudfirebase.appspot.com"
 })
+
 
 # Reference to the database
 ref = db.reference()
@@ -26,17 +28,16 @@ cap.set(3, 640)
 cap.set(4, 480)
 
 # Load the encoding file
-#print("Loading Encode File ...")
+print("Loading Encode File ...")
 file = open('EncodeFile.p', 'rb')
 encodeListKnownWithIds = pickle.load(file)
 file.close()
 encodeListKnown, studentIds = encodeListKnownWithIds
-studentNames = {}
+# print(studentIds)
+print("Encode File Loaded")
 
-# Load student names from Firebase HEER EDIT
-
-
-#print("Encode File Loaded")
+counter = 0
+id = -1
 
 while True:
     success, img = cap.read()
@@ -48,11 +49,13 @@ while True:
     faceCurFrame = face_recognition.face_locations(imgS)
     encodeCurFrame = face_recognition.face_encodings(imgS, faceCurFrame)
 
+
     # Compare the current frame's encodings with the known encodings
     if faceCurFrame:
         for encodeFace, faceLoc in zip(encodeCurFrame, faceCurFrame):
             matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
             faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+
             matchIndex = np.argmin(faceDis)
 
             if matches[matchIndex]:
@@ -64,18 +67,33 @@ while True:
 
                 # Mark the student as present
                 id = studentIds[matchIndex]
-                cv2.imshow(" webcam ", img)
-                cv2.waitKey(1)
-                ref = db.reference(f'Students/{id}')
-                id ['present'] = 'fuls'
 
+                #ref.child(id).update({"present": True})
+                
+                if counter == 0:
+                    cvzone.putTextRect(img, "Loading", (275, 400))
+                    cv2.imshow("Face Attendance", img)
+                    cv2.waitKey(1)
+                    counter = 1
+
+
+                if counter != 0:
+
+                    if counter == 1:
+                        # Get the Data
+                        studentInfo = db.reference(f'Students/{id}').get()
+                        print(studentInfo)
 
 
 
     else:
-        # No faces found, update all students as absent
+                # No faces found, update all students as absent
       for id in studentIds:
         ref.child(id).update({"present": False})
 
     cv2.imshow(" webcam ", img)
     cv2.waitKey(1)
+
+
+
+
