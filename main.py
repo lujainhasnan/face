0047e089f67571150b3c7fa9d0b8c0e2a3b2a7d7
lujainhasnan <121128316+lujainhasnan@users.noreply.github.com>
2023-02-15 09:53:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -61,6 +61,8 @@ while True:
                 y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
                 bbox = 55 + x1, 162 + y1, x2 - x1, y2 - y1
                 img = cvzone.cornerRect(img, bbox, rt=0)
+                cv2.imshow(" webcam ", img)
+                cv2.waitKey(1)
 
                 # Mark the student as present
                 id = studentIds[matchIndex]
@@ -76,5 +78,4 @@ while True:
                ref = db.reference(f'Students/{id}')
                ref.child('attendance').set('present')
 
-               cv2.imshow(" webcam ", img)
-               cv2.waitKey(1)
+               
